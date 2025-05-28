42b6f319fb7977ccc66e8278db70cfb3f9dd9eea
thbueno <thiagobueno.s@gmail.com>
2025-05-28 08:00:00 +0700

    commit catch up

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT =  process.env.PORT || 5000;
 
-
-
+app.get("/",(req, res) => {
+    res.sendFile(path)
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }
