12652c6f1daa8c95dcfa61895883ee6aa95f5105
thbueno <thiagobueno.s@gmail.com>
2025-05-29 08:00:00 +0700

    path of URL front end

--- src/server.js
+++ src/server.js
@@ -5,6 +5,12 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT =  process.env.PORT || 5000;
 
+
+// get the file path from the URL name of the current module
+const __filename = fileURLToPath(import.meta.url);
+// get the directory name of the current module
+const __dirname = dirname(__filename);
+
 app.get("/",(req, res) => {
     res.sendFile(path)
 })
