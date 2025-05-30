bf3cd5078a44870d1c92538bf896dd6844fbf801
thbueno <thiagobueno.s@gmail.com>
2025-05-30 08:00:00 +0700

    Creating tables

--- src/db.js
+++ src/db.js
@@ -13,7 +13,10 @@ db.exec(`
 
 db.exec(`
     CREATE TABLE todos (
-        id INTEGER,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
+        task TEXT,
+        completed BOOLEAN DEFAULT 0,
+        FOREIGN KEY (user_id) REFERENCES user(id)
     )
 `)
