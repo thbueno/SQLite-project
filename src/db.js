36e736f21d2ae813cb4f0db7b6eb2261736a476a
thbueno <thiagobueno.s@gmail.com>
2025-05-30 08:00:00 +0700

    Creating tables

--- src/db.js
+++ src/db.js
@@ -4,4 +4,9 @@ const db = new DatabaseSync(':memory:')
 // Execute SQL statements to create the table
 
 db.exec(`
+    CREATE TABLE user {
+        id INTEGER,
+        username TEXT UNIQUE,
+        password TEXT
+    }
     `)
