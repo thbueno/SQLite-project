1c38c77ad78a24e61e7ad7388145c354d6693721
thbueno <thiagobueno.s@gmail.com>
2025-05-31 09:00:00 +0700

    auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,8 @@
-import express from 'express';
+import express, { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import db from '../db.js';
+
+const router = express.Router();
+
+export default router
