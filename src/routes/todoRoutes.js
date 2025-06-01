eb05b46c7bc6046c61f410e350aaacc719ff40c2
thbueno <thiagobueno.s@gmail.com>
2025-06-01 10:00:00 +0700

    auth routes

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -2,3 +2,9 @@ import express from 'express';
 import db from '../db.js';
 
 const router = express.Router();
+
+// Get all todos for a user
+router.get('/todos', (req, res) => {})
+// Create a new todo for a user
+router.post('/', (req, res) => {})
+
