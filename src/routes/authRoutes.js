676f90108c52161763a3290ec9e956281c0b6fa2
thbueno <thiagobueno.s@gmail.com>
2025-06-04 22:35:15 +0700

    fix authRoutes import react error

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,13 +2,12 @@ import express, { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import db from '../db.js';
-import { use } from 'react';
 
 const router = express.Router();
 
 // Register a new user endpoint auth/register
 router.post('/register', (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
     console.log(username, password);
     res.sendStatus(201)
 
