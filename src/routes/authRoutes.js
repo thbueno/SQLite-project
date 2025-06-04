183782767c4954a41afe9c4690a32e50948e7c9f
thbueno <thiagobueno.s@gmail.com>
2025-06-04 22:23:45 +0700

    register router logic first struture

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,15 @@ import express, { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import db from '../db.js';
+import { use } from 'react';
 
 const router = express.Router();
 
+// Register a new user endpoint auth/register
 router.post('/register', (req, res) => {
+    const {username, password} = req.body;
+    console.log(username, password);
+    res.sendStatus(201)
 
 })
 
