1769d4c097072a8a4c3fde76602e297adc142e03
thbueno <thiagobueno.s@gmail.com>
2025-06-06 11:00:00 +0700

    conig todos routes endpoints

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -6,7 +6,8 @@ const router = express.Router();
 // Get all todos for a user
 router.get('/todos', (req, res) => {
     const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
-    const todos = getTodos.all(req.userID);
+    const todos = getTodos.all(req.userId);
+    res.json(todos);
 })
 
 // Create a new todo for a user
