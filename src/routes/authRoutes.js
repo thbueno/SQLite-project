7104992de3c5a39d19f55790e76bea16409b51d8
thbueno <thiagobueno.s@gmail.com>
2025-06-07 12:54:22 +0700

    res json token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -27,6 +27,7 @@ router.post('/register', (req, res) => {
         const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, {
             expiresIn: 86400 // 24 hours
         });
+        res.json({ token })
 
     } catch (error) {
         console.log(error.message);
