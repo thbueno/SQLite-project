a6bb2bf7eb49a81b9193956d10b45b81753da5bf
thbueno <thiagobueno.s@gmail.com>
2025-06-07 11:00:00 +0700

    configuration of the middleware token

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -6,4 +6,6 @@ function authMiddleware (req, res, next) {
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
     }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {})
 }
