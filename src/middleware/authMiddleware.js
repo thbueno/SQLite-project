4cc0445111b84fcb5b03fa6c5f66e9205a7a0226
thbueno <thiagobueno.s@gmail.com>
2025-06-08 08:00:00 +0700

    configuration of the middleware token

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -9,7 +9,8 @@ function authMiddleware (req, res, next) {
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(401).json({ message: 'invalid token' });
-        }
+            return res.status(401).json({ message: 'invalid token' });}
+            req.userId = decoded.id;
+            next();
     })
 }
