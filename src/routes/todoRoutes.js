df61168867fff2a5143749e25d06bdbc812c3414
thbueno <thiagobueno.s@gmail.com>
2025-06-10 23:36:51 +0700

    clange the authRoutes with the reference

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -4,10 +4,10 @@ import db from '../db.js';
 const router = express.Router();
 
 // Get all todos for a user
-router.get('/todos', (req, res) => {
-    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
-    const todos = getTodos.all(req.userId);
-    res.json(todos);
+router.get('/', (req, res) => {
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
 })
 
 // Create a new todo for a user
