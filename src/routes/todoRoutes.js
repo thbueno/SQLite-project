705d43940d90d49b0902a97dbd6dcffbbae0f7b0
thbueno <thiagobueno.s@gmail.com>
2025-06-10 22:44:05 +0700

    correct the GET route url

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -4,7 +4,7 @@ import db from '../db.js';
 const router = express.Router();
 
 // Get all todos for a user
-router.get('/todos', (req, res) => {
+router.get('/', (req, res) => {
     const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
     const todos = getTodos.all(req.userId);
     res.json(todos);
